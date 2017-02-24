aa07fa28f12fea53b89ec9b8b62274bf00fec350
Viraj Padte <virajpadte@gmail.com>
2017-02-24 13:53:50 -0700

    fixed bugs in login

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -18,9 +18,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
-    
-    let alert = UIAlertController(title: "", message: "", preferredStyle: UIAlertControllerStyle.alert)
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,76 +29,88 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
     @IBAction func signUpProcess(_ sender: Any) {
-        if signupButton.titleLabel?.text == "Sign Up"{
-            print("Sign Up")
-            //add user
-            let user  = PFUser()
-            user.username = username.text
-            user.password = password.text
-            user.signUpInBackground(block: { (saved, error) in
-                if error != nil{
-                    if let error  = error{
-                        let errorMessage = error.localizedDescription
-                        print(errorMessage)
-                        self.alert.title = "Opps"
-                        if errorMessage.contains("already exists"){
-                            self.alert.message = errorMessage
+        //add user
+        let user  = PFUser()
+        if username.text != "" && password.text != ""{
+            if signupButton.titleLabel?.text == "Sign Up"{
+                print("Sign Up")
+                user.username = username.text
+                user.password = password.text
+                user.signUpInBackground(block: { (saved, error) in
+                    if error != nil{
+                        if let error  = error{
+                            let errorMessage = error.localizedDescription
+                            print(errorMessage)
+                            if errorMessage.contains("already exists"){
+                                let alert = UIAlertController(title: "Opps", message: errorMessage, preferredStyle: UIAlertControllerStyle.alert)
+                                alert.addAction(UIAlertAction.init(title: "Ok", style: UIAlertActionStyle.default, handler: { (alerted) in
+                                    alert.dismiss(animated: true, completion: nil)
+                                    }))
+                                self.present(alert, animated: true, completion: nil)
+                            }
+                            else{
+                                let alert = UIAlertController(title: "Opps", message: "Something weird happened. Try sigining up in sometime", preferredStyle: UIAlertControllerStyle.alert)
+                                alert.addAction(UIAlertAction.init(title: "Ok", style: UIAlertActionStyle.default, handler: { (alerted) in
+                                    alert.dismiss(animated: true, completion: nil)
+                                    }))
+                                self.present(alert, animated: true, completion: nil)
+                            }
+                            self.username.text = ""
+                            self.password.text = ""
                         }
-                        else{
-                            self.alert.message = "Something weird happened. Try siginin up in sometime"
-                        }
-                        
-                        self.alert.addAction(UIAlertAction.init(title: "Ok", style: UIAlertActionStyle.default, handler: { (alerted) in
-                        self.alert.dismiss(animated: true, completion: nil)
+                    }
+                    else{
+                        print("Successfull sign up")
+                        //notify the user
+                        let alert = UIAlertController(title: "Success", message: "Thanks for signing up", preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction.init(title: "Ok", style: UIAlertActionStyle.default, handler: { (okPressed) in
+                            alert.dismiss(animated: true, completion: nil)
                         }))
-                        self.present(self.alert, animated: true, completion: nil)
+                        self.present(alert, animated: true, completion: nil)
                         self.username.text = ""
                         self.password.text = ""
                     }
-                    
-                }
-                else{
-                    print("saved")
-                    //notify the user
-                    self.alert.title = "Success"
-                    self.alert.message = "Thanks for signin up"
-                    self.alert.addAction(UIAlertAction.init(title: "Ok", style: UIAlertActionStyle.default, handler: { (okPressed) in
-                        self.alert.dismiss(animated: true, completion: nil)
-                    }))
-                    self.present(self.alert, animated: true, completion: nil)
-                    self.username.text = ""
-                    self.password.text = ""
-                }
-            })
+                })
+            }
+            else{
+                print("Login")
+                //verfiy user
+                PFUser.logInWithUsername(inBackground: username.text!, password: password.text!, block: { (loggedIn, error) in
+                    if error != nil{
+                        print(error!.localizedDescription)
+                        //notify the user
+                        let alert = UIAlertController(title: "Opps", message: error!.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction.init(title: "Ok", style: UIAlertActionStyle.default, handler: { (okPressed) in
+                            alert.dismiss(animated: true, completion: nil)
+                        }))
+                        self.present(alert, animated: true, completion: nil)
+                        self.username.text = ""
+                        self.password.text = ""
+                    }
+                    else if let loggedInUser = loggedIn{
+                        print(loggedInUser)
+                        //notify the user
+                        print("User logged in")
+                        
+                    }
+                })
+            }
         }
         else{
-            print("Login")
-            //verfiy user
-            PFUser.logInWithUsername(inBackground: username.text!, password: password.text!, block: { (loggedIn, error) in
-                if error != nil{
-                    print(error?.localizedDescription)
-                    //notify the user
-                    self.alert.title = "Opps"
-                    self.alert.message = error?.localizedDescription
-                    self.alert.addAction(UIAlertAction.init(title: "Ok", style: UIAlertActionStyle.default, handler: { (okPressed) in
-                        self.alert.dismiss(animated: true, completion: nil)
-                    }))
-                    self.present(self.alert, animated: true, completion: nil)
-                    self.username.text = ""
-                    self.password.text = ""
-                }
-                else if let loggedInUser = loggedIn{
-                    print(loggedInUser)
-                    //notify the user
-                    print("User logged in")
-
-                }
-            })
-           
-            
+            //notify the user
+            let alert = UIAlertController(title: "Opps", message: "Invalid username/password", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction.init(title: "Ok", style: UIAlertActionStyle.default, handler: { (okPressed) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+            self.username.text = ""
+            self.password.text = ""
         }
     }
+    
     @IBAction func loginProcess(_ sender: Any) {
         if newUser == false{
             loginButton.setTitle("Sign Up", for: .normal)
